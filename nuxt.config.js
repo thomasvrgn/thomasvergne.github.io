28b9974a2e37735a875f1ad52e329617e1135b6d
NessMC <contact@nessmc.fr>
2020-12-09 18:28:45 +0100

    🐛 Nuxt configuration file fixed.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-  mode: 'spa',
+  ssr: false,
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
