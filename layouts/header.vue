ad0e703e42ac228e223ca3bcb70ebe4049c9e6c8
thomasvergne <thomas@quark-lang.dev>
2020-12-10 18:47:38 +0100

    💄 Header style added.

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,16 +1,16 @@
 <template>
-  <main class="flex flex-col">
-    <header class="bg-blue-300" style="height: 450px">
-      test
+  <main class="flex flex-col lg:min-h-screen">
+    <header class="bg-red-400 flex flex-auto" style="height: 450px">
+      Header
     </header>
-    <section class="flex flex-col">
-      <article class="h-64 bg-red-300">
+    <section class="bg-blue-400 flex flex-col lg:flex-row lg:flex-grow-0 lg:h-64">
+      <article class="h-64 bg-blue-400 lg:w-1/3">
         test
       </article>
-      <article class="h-64 bg-purple-300">
+      <article class="h-64 bg-blue-400 lg:w-1/3">
         test
       </article>
-      <article class="h-64 bg-green-300">
+      <article class="h-64 bg-blue-400 lg:w-1/3">
         test
       </article>
     </section>
