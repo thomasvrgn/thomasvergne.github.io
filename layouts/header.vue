2000966a91352e2bac246579fe9a8328f809585d
thomasvergne <thomas@quark-lang.dev>
2020-12-10 18:22:40 +0100

    📱 Default telephone layout added.

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,5 +1,20 @@
 <template>
-
+  <main class="flex flex-col">
+    <header class="bg-blue-300" style="height: 450px">
+      test
+    </header>
+    <section class="flex flex-col">
+      <article class="h-64 bg-red-300">
+        test
+      </article>
+      <article class="h-64 bg-purple-300">
+        test
+      </article>
+      <article class="h-64 bg-green-300">
+        test
+      </article>
+    </section>
+  </main>
 </template>
 
 <script>
