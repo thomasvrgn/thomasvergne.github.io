24da83550d426cd7ccc50f530cb326f77a73b1f8
thomasvergne <thomas@quark-lang.dev>
2020-12-10 20:18:35 +0100

    💄 Navbar style added.

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -1,5 +1,26 @@
 <template>
-
+  <nav class="p-4 py-6 flex flex-row w-full h-24 container mx-auto">
+    <span class="flex items-center">
+      <img src="~/assets/signature.png" alt="" />
+    </span>
+    <ul class="flex pr-4 flex-auto flex-row space-x-8 justify-end items-center font-semibold text-gray-800">
+      <li>
+        <a href="">
+          test
+        </a>
+      </li>
+      <li>
+        <a href="">
+          test
+        </a>
+      </li>
+      <li>
+        <a href="">
+          test
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
