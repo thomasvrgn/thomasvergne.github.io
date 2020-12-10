da2df85f776fd62b065287f0058deac7aa295419
thomasvergne <thomas@quark-lang.dev>
2020-12-10 20:03:19 +0100

    🚨 IDE warnings fixed.

--- layouts/header.vue
+++ layouts/header.vue
@@ -5,13 +5,13 @@
     </header>
     <section class="bg-blue-400 flex flex-col lg:flex-row lg:flex-grow-0 lg:h-64">
       <article class="h-64 bg-gray-200 lg:w-1/3">
-        <img src="~/assets/entasia.png" class="pt-12 px-8 object-cover object-top h-full w-full" />
+        <img src="~/assets/entasia.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
       </article>
       <article class="h-64 bg-gray-50 lg:w-1/3">
-        <img src="~/assets/palamazon.png" class="pt-12 px-8 object-cover object-top h-full w-full" />
+        <img src="~/assets/palamazon.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
       </article>
       <article class="h-64 bg-gray-100 lg:w-1/3">
-        <img src="~/assets/graphix.png" class="pt-12 px-8 object-cover object-top h-full w-full" />
+        <img src="~/assets/graphix.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
       </article>
     </section>
   </main>
