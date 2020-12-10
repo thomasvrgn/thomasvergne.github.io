79a5bdb5596d02fca111e5a530338942422a85fd
thomasvergne <thomas@quark-lang.dev>
2020-12-10 20:02:06 +0100

    💄 Header project section style added.

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,17 +1,17 @@
 <template>
   <main class="flex flex-col lg:min-h-screen">
-    <header class="bg-red-400 flex flex-auto" style="height: 450px">
+    <header class="bg-white flex flex-auto" style="height: 450px">
       Header
     </header>
     <section class="bg-blue-400 flex flex-col lg:flex-row lg:flex-grow-0 lg:h-64">
-      <article class="h-64 bg-blue-400 lg:w-1/3">
-        test
+      <article class="h-64 bg-gray-200 lg:w-1/3">
+        <img src="~/assets/entasia.png" class="pt-12 px-8 object-cover object-top h-full w-full" />
       </article>
-      <article class="h-64 bg-blue-400 lg:w-1/3">
-        test
+      <article class="h-64 bg-gray-50 lg:w-1/3">
+        <img src="~/assets/palamazon.png" class="pt-12 px-8 object-cover object-top h-full w-full" />
       </article>
-      <article class="h-64 bg-blue-400 lg:w-1/3">
-        test
+      <article class="h-64 bg-gray-100 lg:w-1/3">
+        <img src="~/assets/graphix.png" class="pt-12 px-8 object-cover object-top h-full w-full" />
       </article>
     </section>
   </main>
