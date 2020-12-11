23cef807e11a9e57bafd69541b1d0297f7b8c2b4
thomasvergne <thomas@quark-lang.dev>
2020-12-11 13:35:06 +0100

    💫 Navbar logo animation added.

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="p-4 py-6 flex flex-row w-full h-24 container mx-auto">
     <span class="flex items-center">
-      <img src="~/assets/signature.png" alt="" />
+      <img src="~/assets/signature.png" class="transition-transform duration-300 transform hover:scale-110" alt="" />
     </span>
     <ul class="flex pr-4 flex-auto flex-row space-x-8 lg:space-x-16 justify-end items-center font-semibold text-indigo-900">
       <li>
