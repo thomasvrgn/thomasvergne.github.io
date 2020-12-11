fa9e3681e8d2fdee325999e0eae18bec497a5e29
thomasvergne <thomas@quark-lang.dev>
2020-12-11 12:47:38 +0100

    💄 Section border added.

--- layouts/header.vue
+++ layouts/header.vue
@@ -22,14 +22,14 @@
         </div>
       </div>
     </header>
-    <section class="bg-blue-400 flex flex-col lg:flex-row lg:flex-grow-0 lg:h-64">
-      <article class="h-64 bg-gray-200 lg:w-1/3">
+    <section class="bg-indigo-400 pt-1 flex flex-col lg:flex-row lg:flex-grow-0">
+      <article class="h-full bg-gray-200 lg:w-1/3">
         <img src="~/assets/entasia.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
       </article>
-      <article class="h-64 bg-gray-50 lg:w-1/3">
+      <article class="h-full bg-gray-50 lg:w-1/3">
         <img src="~/assets/palamazon.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
       </article>
-      <article class="h-64 bg-gray-100 lg:w-1/3">
+      <article class="h-full bg-gray-100 lg:w-1/3">
         <img src="~/assets/graphix.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
       </article>
     </section>
@@ -45,3 +45,11 @@ export default {
   }
 }
 </script>
+
+<style>
+@media (min-width: 1024px) {
+  section {
+    height: 340px !important;
+  }
+}
+</style>
