f476a23444dad557ee013f4d628b3a969dece7db
thomasvergne <thomas@quark-lang.dev>
2020-12-12 14:21:25 +0100

    💄 Header style updated.

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,39 +1,23 @@
 <template>
-  <main class="flex flex-col lg:min-h-screen">
-    <header class="bg-white flex flex-auto flex-col" style="height: 450px">
-      <Navbar />
-      <div class="container mx-auto py-4 flex flex-col items-center justify-center flex-auto h-full px-8">
-        <div class="flex justify-center flex-col">
-          <h1 class="text-3xl font-black text-indigo-900">
-            <span class="text-4xl">Thomas</span><br />
-            Développeur Web Front-end
-          </h1>
-          <p class="text-xl mt-4 text-gray-600 text-opacity-75 lg:w-1/2">
-            Je suis développeur web et concepteur d'interfaces graphiques depuis maintenant 5 ans. Je prends beaucoup de plaisir à exercer cette passion.
-          </p>
-          <div class="container flex flex-row mt-4 space-x-8">
-            <button class="flex justify-center bg-indigo-400 flex-initial w-44 py-2 rounded-md text-white focus:outline-none hover:bg-indigo-500">
-              En découvrir plus
-            </button>
-            <button class="flex justify-center items-center text-indigo-400 hover:text-indigo-500">
-              Me contacter
-            </button>
-          </div>
-        </div>
+  <header class="py-12">
+    <div class="w-3/4 mx-auto text-center">
+      <h1 class="font-serif font-bold text-5xl text-gray-700">
+        Thomas<br />
+        Développeur web
+      </h1>
+      <p class="mt-4 font-light text-gray-500 text-lg">
+        Jeune développeur passionné par l'informatique, la programmation ainsi que la création et conception de sites internets depuis maintenant 5 ans.
+      </p>
+      <div class="container mt-4 flex flex-row justify-center space-x-8">
+        <button class="flex transition-colors duration-150 bg-indigo-400 hover:bg-indigo-500 text-white py-2 px-9 font-semibold rounded-full items-center">
+          Découvrir
+        </button>
+        <button class="flex text-indigo-400 transition-colors duration-150 hover:text-indigo-500 font-semibold items-center">
+          Me contacter
+        </button>
       </div>
-    </header>
-    <section class="bg-indigo-400 pt-1 flex flex-col lg:flex-row lg:flex-grow-0">
-      <article class="h-full bg-gray-200 lg:w-1/3">
-        <img src="~/assets/entasia.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
-      </article>
-      <article class="h-full bg-gray-50 lg:w-1/3">
-        <img src="~/assets/palamazon.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
-      </article>
-      <article class="h-full bg-gray-100 lg:w-1/3">
-        <img src="~/assets/graphix.png" class="pt-12 px-8 object-cover object-top h-full w-full"  alt=""/>
-      </article>
-    </section>
-  </main>
+    </div>
+  </header>
 </template>
 
 <script>
