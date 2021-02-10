285862b3d9b536fa783d01967a24840c75ed4392
NessMC <contact@nessmc.fr>
2021-02-10 20:19:19 +0100

    ✨ Main file added.

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
