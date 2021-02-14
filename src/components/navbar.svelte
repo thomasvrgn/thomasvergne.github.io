8f06bd6a8e845dfd4e3e2e480906531594234fa8
NessMC <contact@nessmc.fr>
2021-02-14 16:36:05 +0100

    💄 Blob SVG added.

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,9 +1,13 @@
 <script>
   import Logo from '../assets/logo.png';
 </script>
-
-<nav class="p-4 lg:p-8 lg:px-0">
-    <div class="flex flex-row lg:justify-center lg:w-2/3 lg:mx-auto">
+<div class="absolute right-0 w-64 lg:w-2/6 transform scale-150 z-0 -top-24">
+    <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" width="100%" id="blobSvg">
+        <path id="blob" d="M450,285.5Q446,321,432.5,356.5Q419,392,389.5,416.5Q360,441,324,453Q288,465,248,478Q208,491,168.5,475.5Q129,460,96,433.5Q63,407,56.5,365Q50,323,30.5,286.5Q11,250,30.5,213.5Q50,177,70,146.5Q90,116,114,85.5Q138,55,174,38.5Q210,22,251,16Q292,10,331,26.5Q370,43,398.5,72Q427,101,454,133.5Q481,166,467.5,208Q454,250,450,285.5Z" fill="#f5f5f5"></path>
+    </svg>
+</div>
+<nav class="p-4 lg:p-8 lg:px-0 z-10 relative">
+    <div class="flex flex-row lg:justify-center xl:w-2/3 lg:w-5/6 lg:mx-auto">
         <div class="container flex-row flex flex-initial w-32 lg:justify-center">
             <img src={Logo} class="rounded-full w-16 h-16 shadow-lg" alt="Thomas's logo" />
         </div>
