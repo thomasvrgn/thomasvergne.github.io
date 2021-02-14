10c3bf296b7d5d56b9e12c3ddcb78ad1d5fb62b0
NessMC <contact@nessmc.fr>
2021-02-14 16:26:05 +0100

    ✨ Welcome part added.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
-<div class="container">
-	<h1 class="text-4xl">
-
+<div class="container px-6">
+	<h1 class="text-4xl font-semibold text-gray-800">
+		Présentation
 	</h1>
+	<p class="mt-4 text-gray-800 text-opacity-75 text-lg">
+		Je m'appelle Thomas et suis un passionné de l'informatique et plus particulièrement de la programmation depuis mon plus jeune âge.
+		<br /><br />
+		J'ai pu durant ces 5 dernières années de pratique, expérimenter diverses technologies et divers langages de programmation afin de me spécialiser dans certains domaines.
+	</p>
 </div>
