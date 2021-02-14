fc04682f8cbc0195e9200813deb922679ab80600
NessMC <contact@nessmc.fr>
2021-02-14 16:36:20 +0100

    📱 Presentation responsive section added.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
-<div class="container px-6">
+<div class="container px-6 lg:w-5/6 lg:mx-auto xl:w-2/3">
 	<h1 class="text-4xl font-semibold text-gray-800">
 		Présentation
 	</h1>
-	<p class="mt-4 text-gray-800 text-opacity-75 text-lg">
+	<p class="mt-4 text-gray-800 text-opacity-75 text-lg lg:w-2/3">
 		Je m'appelle Thomas et suis un passionné de l'informatique et plus particulièrement de la programmation depuis mon plus jeune âge.
 		<br /><br />
 		J'ai pu durant ces 5 dernières années de pratique, expérimenter diverses technologies et divers langages de programmation afin de me spécialiser dans certains domaines.
