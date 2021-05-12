c34adfd4ac2f2fc6f3bac1a4b33cf3468549363e
thomasvergne <thomas@quark-lang.dev>
2021-05-13 01:58:07 +0200

    💄 Presentation component added.

---
+++ src/components/Presentation.js
@@ -0,0 +1,18 @@
+import { Title } from './utils/Title';
+
+export const Presentation = () => {
+  return <section>
+    <Title>
+      Présentation
+    </Title>
+    <p className="mt-4 text-gray-600 text-opacity-90 dark:text-white dark:text-opacity-60">
+      Je m'appelle Thomas et ai actuellement 15 ans. Je suis passionné désormais depuis 5 ans par l'informatique et un peu moins de deux ans par les mathématiques. J'ai commencé la conception de langages en cette année 2020.<br /><br />
+
+      <span className="text-lg font-medium text-green-800 dark:text-green-300">
+        N'hésitez pas à me poser des questions depuis mes moyens de contact...
+      </span><br/><br/>
+
+      Concernant mon parcours web, j'ai pu expérimenter divers bords et diverses approches comme le côté serveur, côté database, côté devops, mais ai préféré le côté client (front end) pour la créativité que ce dernier requiert.
+    </p>
+  </section>
+}
