40636c27de631947f7d761a01a2d4bc29ad0fefd
thomasvergne <thomas@quark-lang.dev>
2021-05-13 02:36:18 +0200

    💫 Skills animation added.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,25 +1,23 @@
 import { Title } from './utils/Title';
 import { skills } from './data/skills.data';
 
-export const Skills = () => {
-  return <section>
-    <Title>Compétences</Title>
-    <ul className="px-4 py-8 space-y-6">
-      {skills.map(({title, subtitle, image}) => (
-        <Item image={image}>
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
-        </Item>
-      ))}
-    </ul>
-  </section>
-}
+export const Skills = () => <section>
+  <Title>Compétences</Title>
+  <ul className="px-4 pt-8 space-y-6">
+    {skills.map(({title, subtitle, image}) => (
+      <Item image={image}>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </Item>
+    ))}
+  </ul>
+</section>
 
 export const Item = ({ image, children }) => {
   const [title, subtitle] = children;
   return (
-    <li className="flex flex-row items-center flex-wrap">
-      <img src={image} alt="Item" className="flex-initial w-1/12" />
+    <li className="flex flex-row items-center flex-wrap group">
+      <img src={image} alt="Item" className="flex-initial w-1/12 transform transition-transform duration-1000 group-hover:scale-[1.2]" />
       <span className="ml-4 text-lg font-medium dark:text-white text-gray-700 flex-auto w-10/12">
         {title}
       </span>
