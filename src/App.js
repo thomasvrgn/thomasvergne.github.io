35eac50ca8818052b9dc634f0d6aa906381f9d60
thomasvergne <thomas@quark-lang.dev>
2021-05-13 15:10:22 +0200

    ➕ Projet and contact components imported into app.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Navbar } from './components/Navbar';
 import { Presentation } from './components/Presentation';
 import { Skills } from './components/Skills';
 import { Features } from './components/Features';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
 
 export const App = () => {
   return <main className="dark:bg-gray-800">
@@ -10,6 +12,8 @@ export const App = () => {
       <Presentation />
       <Skills />
       <Features />
+      <Projects />
+      <Contact />
     </div>
   </main>
 }
