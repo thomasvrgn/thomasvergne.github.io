75552f57e5731a91db4b7eecba684412633c9d86
thomasvergne <thomas@quark-lang.dev>
2021-05-13 02:36:28 +0200

    ➕ Features component imported into App.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Navbar } from './components/Navbar';
 import { Presentation } from './components/Presentation';
 import { Skills } from './components/Skills';
+import { Features } from './components/Features';
 
 export const App = () => {
   return <main className="dark:bg-gray-800">
@@ -8,6 +9,7 @@ export const App = () => {
     <div className="space-y-16 px-4 mt-8">
       <Presentation />
       <Skills />
+      <Features />
     </div>
   </main>
 }
