affac8a2e5894549598af1a72c45c9eba81a68c3
thomasvergne <thomas@quark-lang.dev>
2021-05-13 18:21:04 +0200

    🔧 Blank button href target property added.

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,7 +1,7 @@
-export const Button = ({ children, coloured = false, href }) => (
-  <button className={`${coloured ? 'bg-emerald-500 text-white hover:bg-emerald-600' : 'bg-white text-gray-800 hover:bg-gray-100'} py-2 px-8 rounded-lg shadow-lg font-medium focus:outline-none transition-colors duration-200`} onClick={() => goTo(href)}>
+export const Button = ({ children, coloured = false, href, blank = false, }) => (
+  <button className={`${coloured ? 'bg-emerald-500 text-white hover:bg-emerald-600' : 'bg-white text-gray-800 hover:bg-gray-100'} py-2 px-8 rounded-lg shadow-lg font-medium focus:outline-none transition-colors duration-200`} onClick={() => goTo(href, blank)}>
     {children}
   </button>
 );
 
-const goTo = (link) => window.open(link, '_blank');
+const goTo = (link, blank) => window.open(link, blank ? '_blank' : '_self')
