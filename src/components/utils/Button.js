348cc8b60cd7ea9b3fd50854829fd71bfed31e27
thomasvergne <thomas@quark-lang.dev>
2021-05-13 15:09:32 +0200

    🔧 Button href property added.

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,5 +1,7 @@
-export const Button = ({ children, coloured = false, }) => (
-  <button className={`${coloured ? 'bg-green-400 text-white' : 'bg-white text-gray-800'} py-2 px-8 rounded-lg shadow-lg font-medium`}>
+export const Button = ({ children, coloured = false, href }) => (
+  <button className={`${coloured ? 'bg-emerald-500 text-white hover:bg-emerald-600' : 'bg-white text-gray-800 hover:bg-gray-100'} py-2 px-8 rounded-lg shadow-lg font-medium focus:outline-none transition-colors duration-200`} onClick={() => goTo(href)}>
     {children}
   </button>
-)
+);
+
+const goTo = (link) => window.open(link, '_blank');
