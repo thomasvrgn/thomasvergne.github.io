394f74c3323d79f29b19772d896e041f289af13c
thomasvergne <thomas@quark-lang.dev>
2021-05-14 16:22:08 +0200

    💄 App main entry padding fixed.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { Skills } from './components/Skills';
 import { Features } from './components/Features';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 
 export const App = () => {
   return <main className="dark:bg-gray-800">
     <Navbar />
-    <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
-      <Presentation />
-      <Skills />
-      <Features />
-      <Projects />
-      <Contact />
+    <div>
+      <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
+        <Presentation />
+        <Skills />
+        <Features />
+        <Projects />
+        <Contact />
+      </div>
+      <Footer />
     </div>
   </main>
 }
