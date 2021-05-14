0ef477e281f0a5ab1f20fdf3df91d22bbf3bd73a
thomasvergne <thomas@quark-lang.dev>
2021-05-14 14:42:17 +0200

    ✨ Button disabled property added.

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,5 +1,5 @@
-export const Button = ({ children, coloured = false, href, blank = false, }) => (
-  <button className={`${coloured ? 'bg-emerald-500 text-white hover:bg-emerald-600' : 'bg-white text-gray-800 hover:bg-gray-100'} py-2 px-8 rounded-lg shadow-lg font-medium focus:outline-none transition-colors duration-200`} onClick={() => goTo(href, blank)}>
+export const Button = ({ children, coloured = false, href, blank = false, full = false, disabled = false }) => (
+  <button className={`${disabled ? 'bg-gray-300 text-gray-800 pointer-events-none' : coloured ? 'bg-emerald-500 text-white hover:bg-emerald-600' : 'bg-white text-gray-800 hover:bg-gray-100'} py-2 px-8 rounded-lg shadow-lg font-medium focus:outline-none transition-colors duration-200 ${full ? 'w-full' : ''}`} onClick={() => goTo(href, blank)} disabled={disabled}>
     {children}
   </button>
 );
