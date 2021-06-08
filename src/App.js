1958ad60044b1533fd186f7bab4c043bba8fa785
thomasvergne <thomas@quark-lang.dev>
2021-06-09 00:52:58 +0200

    👽 React router transition started.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,36 @@ import { Features } from './components/Features';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { Blog } from './Blog';
+import { Article } from './Article';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 export const App = () => {
   return <main className="dark:bg-gray-800">
-    <Navbar />
-    <div>
-      <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
-        <Presentation />
-        <Skills />
-        <Features />
-        <Projects />
-        <Contact />
-      </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/blog/:slug">
+          <Article />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/">
+          <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
+            <Presentation />
+            <Skills />
+            <Features />
+            <Projects />
+            <Contact />
+          </div>
+        </Route>
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   </main>
 }
