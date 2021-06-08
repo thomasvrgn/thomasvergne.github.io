56175241b0020390a27c16f8a33cf768fdd84174
thomasvergne <thomas@quark-lang.dev>
2021-06-08 23:56:40 +0200

    🔥 Removed all useless files.

--- src/components/utils/Button.js
+++
@@ -1,9 +0,0 @@
-export const Button = ({ children, coloured = false, href, blank = false, full = false, disabled = false }) => (
-  <div className={disabled ? 'cursor-not-allowed' : 'cursor-pointer'}>
-    <button className={`${disabled ? 'dark:bg-gray-300 cursor-text bg-gray-200 border-gray-400 border text-gray-800 pointer-events-none' : coloured ? 'bg-emerald-500 text-white hover:bg-emerald-600' : 'bg-white text-gray-800 hover:bg-gray-100 border-gray-400 border dark:border-none'} py-2 px-8 rounded-lg dark:border-none dark:shadow-lg font-medium focus:outline-none transition-colors duration-200 ${full ? 'w-full' : ''}`} onClick={() => goTo(href, blank)} disabled={disabled}>
-      {children}
-    </button>
-  </div>
-);
-
-const goTo = (link, blank) => window.open(link, blank ? '_blank' : '_self')
--- src/components/utils/Title.js
+++
@@ -1,5 +0,0 @@
-export const Title = ({ children }) => (
-  <h1 className="text-4xl font-semibold text-gray-800 dark:text-white">
-    {children}
-  </h1>
-)
