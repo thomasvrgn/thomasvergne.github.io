b4f03ad91e2841e3f78ce1ad9337225e5b70b5a4
thomasvergne <thomas@quark-lang.dev>
2021-06-09 17:21:59 +0200

    🐛 Fixed new import paths.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { Navbar } from './components/Navbar';
-import { Presentation } from './components/Presentation';
-import { Skills } from './components/Skills';
-import { Features } from './components/Features';
-import { Projects } from './components/Projects';
-import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-import { Blog } from './Blog';
-import { Article } from './Article';
+import { Blog } from './pages/Blog';
+import { Article } from './pages/Article';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
-import { Author } from './Author';
+import { Author } from './pages/Author';
+import { Home } from './pages/Main';
 
 export const App = () => {
   return <main className="dark:bg-gray-800">
@@ -29,13 +25,7 @@ export const App = () => {
           <Blog />
         </Route>
         <Route path="/">
-          <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
-            <Presentation />
-            <Skills />
-            <Features />
-            <Projects />
-            <Contact />
-          </div>
+          <Home />
         </Route>
       </Switch>
       <Footer />
