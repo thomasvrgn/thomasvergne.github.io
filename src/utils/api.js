73bdd0580aff97aecd1b5b9ac0a77741358061f3
thomasvergne <thomas@quark-lang.dev>
2021-06-12 18:04:42 +0200

    👽 Added GitHub and preloaded blog API.

---
+++ src/utils/api.js
@@ -0,0 +1,9 @@
+import { Octokit } from '@octokit/rest';
+import { fetchRouter } from './article';
+
+export const { REACT_APP_API_TOKEN } = process.env;
+export const octo = new Octokit({
+  auth: REACT_APP_API_TOKEN,
+});
+
+export const articles = fetchRouter();
