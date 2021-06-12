9a39a94ff1990af181787845c51eb12c1f62625e
thomasvergne <thomas@quark-lang.dev>
2021-06-12 18:03:56 +0200

    🐛 Fixed document title main page.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,12 +4,13 @@ import { Features } from '../components/Features';
 import { Projects } from '../components/Projects';
 import { Contact } from '../components/Contact';
 
-export const Home = () => (
-  <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
+export const Home = () => {
+  document.title = 'Thomas Vergne'
+  return <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
     <Presentation />
     <Skills />
     <Features />
     <Projects />
     <Contact />
   </div>
-)
+}
