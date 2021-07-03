3cdb7c8dfb0417ae15a2c991121f106bc45c8364
NessMC <contact@nessmc.fr>
2021-07-03 12:02:46 +0200

    ✨ Added error page.

---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+export const Error = () => <section className="m-8">
+  <div className="mx-4 py-8 lg:w-1/2 lg:mx-auto lg:text-center lg:my-32 dark:text-white text-gray-700">
+    <h1 className="text-xl font-semibold">Erreur lors du chargement de la page</h1>
+    <p className="opacity-50 font-light lg:w-1/2 lg:mx-auto">
+      La page demandée n'existe pas.<br />
+      Vérifiez si une faute n'a pas été commise dans l'URL. Si le problème persiste, contactez l'administrateur :
+    </p> 
+    <a href="mailto:thomas@quark-lang.dev" className="text-emerald-400 dark:text-emerald-300">
+      thomas@quark-lang.dev
+    </a>
+  </div>
+</section>
