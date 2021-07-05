406b4accf0dc7332afbb4237cae59ff08b8816c7
thomasvergne <thomas@quark-lang.dev>
2021-07-05 12:08:38 +0200

    💥 Tickets layout updated.

--- src/pages/dashboard/Tickets.js
+++ src/pages/dashboard/Tickets.js
@@ -74,10 +74,10 @@ export const Tickets = ({ session }) => {
   return order 
     ? <div>
         <div className="flex flex-col md:flex-row mb-6 space-y-8 md:space-y-0">
-          <h1 className="text-2xl font-bold text-white flex-auto">
+          <h1 className="text-2xl font-bold text-gray-800 dark:text-white flex-auto">
             Tickets
           </h1>
-          <div className="bg-red-200 bg-opacity-90 rounded-2xl p-4 px-6 pr-10 pb-5 flex flex-row items-center text-red-900 md:w-max">
+          <div className="bg-red-100 dark:bg-red-200 bg-opacity-90 rounded-2xl p-4 px-6 pr-10 pb-5 flex flex-row items-center text-red-900 md:w-max">
             <FontAwesomeIcon className="text-2xl mt-2.5" icon={faExclamationTriangle} />
             <div className="ml-4">
               <h1 className="text-lg font-bold">Attention !</h1>
@@ -85,42 +85,44 @@ export const Tickets = ({ session }) => {
             </div>
           </div>
         </div>
-        <div className="space-y-8 flex flex-col lg:flex-row lg:space-y-0 lg:gap-8 flex-wrap items">
-          {(order && order.tickets.length > 0) && order.tickets.map((x, i) => {
-            const [title, content] = x.split(':');
-            return <div className="bg-gray-900 p-8 relative rounded-2xl shadow-2xl lg:w-[47%] xl:w-[31%]" key={i}>
-              <span className="text-xl font-medium text-white">{title}</span>
-              <p className="text-lg text-white opacity-70 leading-5 mt-2">{content}</p>
-              <button className="text-white text-xl opacity-75 focus:outline-none p-8 absolute right-0 top-0" onClick={() => removeTicket(session, id, i, [order, setOrder])}>
-                <FontAwesomeIcon icon={faTimes} />
-              </button>
-            </div>
-          })}
-          {(!order || order.tickets.length === 0) && <p className="text-lg text-white font-medium">
-              Aucun ticket trouvé
-            </p>}
-        </div>
-        <form onSubmit={e => { e.preventDefault(); pushTicket(session, id, title, subtitle, [order, setOrder]) }} className="bg-gray-900 bg-opacity-50 p-8 space-y-4 rounded-2xl mt-24 shadow-xl lg:mx-auto lg:w-1/2">
-          <header>
-            <h1 className="text-3xl font-bold text-white">
-              Créer un ticket
-            </h1>
-            <p className="text-white text-opacity-60 w-3/4">
-              Composez et envoyez dès à présent votre demande...
-            </p>
-          </header>
-          <Input ref={title}>
-            Titre
-          </Input>
-          <Input textarea ref={subtitle}>
-            Décrivez votre demande
-          </Input>
-          <Button coloured full>
-            Envoyer votre ticket
-          </Button>
-        </form>
+        <section className="lg:flex lg:flex-row lg:gap-x-6">
+          <div className="space-y-8 flex flex-col lg:flex-row lg:space-y-0 lg:gap-8 flex-wrap items lg:flex-auto h-full">
+            {(order && order.tickets.length > 0) && order.tickets.map((x, i) => {
+              const [title, content] = x.split(':');
+              return <div className="dark:bg-gray-900 bg-gray-50 p-8 relative rounded-2xl dark:shadow-2xl md:w-full 2xl:w-[47%]" key={i}>
+                <span className="text-xl font-medium dark:text-white text-gray-800">{title}</span>
+                <p className="text-lg text-gray-800 dark:text-white opacity-70 leading-5 mt-2">{content}</p>
+                <button className="text-gray-800 dark:text-white text-xl opacity-75 focus:outline-none m-8 absolute right-0 top-0" onClick={() => removeTicket(session, id, i, [order, setOrder])}>
+                  <FontAwesomeIcon icon={faTimes} />
+                </button>
+              </div>
+            })}
+            {(!order || order.tickets.length === 0) && <p className="text-lg text-gray-800 dark:text-white font-medium">
+                Aucun ticket trouvé
+              </p>}
+          </div>
+          <form onSubmit={e => { e.preventDefault(); pushTicket(session, id, title, subtitle, [order, setOrder]) }} className="border dark:border-none dark:bg-gray-900 bg-opacity-50 p-8 space-y-4 rounded-2xl mt-24 dark:shadow-xl lg:mx-auto lg:mt-0 lg:h-[min-content]">
+            <header className="text-gray-800 dark:text-white">
+              <h1 className="text-3xl font-bold">
+                Créer un ticket
+              </h1>
+              <p className="text-opacity-60 w-3/4">
+                Composez et envoyez dès à présent votre demande...
+              </p>
+            </header>
+            <Input ref={title}>
+              Titre
+            </Input>
+            <Input textarea ref={subtitle}>
+              Décrivez votre demande
+            </Input>
+            <Button coloured full>
+              Envoyer votre ticket
+            </Button>
+          </form>
+        </section>
       </div>
-    : <p className="text-lg text-white font-medium">
+    : <p className="text-lg text-gray-800 dark:text-white font-medium">
       Aucune commande n'a été trouvée !  
     </p>
 }
