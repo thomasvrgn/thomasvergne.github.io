f2bba8d8737c9afd185239d01e5403765a16aae4
NessMC <contact@nessmc.fr>
2021-08-05 15:20:00 +0200

    ➕ Added experiences section to main page.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,13 +3,15 @@ import { Skills } from '../components/Skills';
 import { Features } from '../components/Features';
 import { Projects } from '../components/Projects';
 import { Contact } from '../components/Contact';
+import { Experiences } from '../components/Experiences'; 
 
 export const Home = () => {
-  document.title = 'Thomas Vergne'
+  document.title = 'Thomas Vergne';
   return <div className="space-y-16 px-4 mt-8 lg:w-4/5 md:w-[90%] mx-auto">
     <Presentation />
     <Skills />
     <Features />
+    <Experiences />
     <Projects />
     <Contact />
   </div>
