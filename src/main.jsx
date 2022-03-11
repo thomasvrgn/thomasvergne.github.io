3d158455ee77647b4f4d1bac9c50bac1ba83a979
thomasvergne <thomas.vergne.contact@gmail.com>
2022-03-11 15:19:24 +0100

    🔧 Updated imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Header from './Header';
+import Projects from './Projects';
 import 'styles/tailwind.css';
+import Button from 'components/Button';
+import Blog from './Blog';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Projects />
+    <Blog />
+    <div className="p-16">
+      <Button onClick={() => window.localStorage.clear()}>
+        Supprimer le cache
+      </Button>  
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 )
