d8fdba14c1cb99d8f481a8832d713bd9a32410d2
thomasvergne <thomas.vergne.contact@gmail.com>
2022-03-11 15:18:23 +0100

    ✨ Added text utility components.

---
+++ components/Typography.jsx
@@ -0,0 +1,15 @@
+export const HeadTitle = ({ children }) =>
+  <h2 className="font-black text-5xl text-black flex items-center">
+    {children}
+    <span className="block h-0.5 flex-auto mt-2 ml-16 bg-black opacity-10"></span>
+  </h2>;
+
+export const Title = ({ children }) =>
+  <h3 className="font-black text-xl text-black">
+    {children}
+  </h3>;
+
+export const Paragraph = ({ children }) =>
+  <p className="text-lg text-black text-opacity-60">
+    {children}
+  </p>;
