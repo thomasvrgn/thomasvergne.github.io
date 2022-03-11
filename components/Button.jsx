286b18baf009f871cc1ee7967a15ca66841a4d2d
thomasvergne <thomas.vergne.contact@gmail.com>
2022-03-11 15:18:14 +0100

    ✨ Added Button component.

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,8 @@
-export default function Button({ children, withIcon, ...props }) {
-  const Icon = withIcon;
-  return <button className="group hover:shadow-xl transition-all duration-500 rounded-md py-2 px-8 flex items-center mx-auto !bg-black text-white font-medium tracking-wide" {...props}>
+import Icon from 'components/Icon';
+
+export default function Button({ children, withIcon, black, ...props }) {
+  return <button className={`group rounded-md py-2 px-8 flex items-center font-medium tracking-wide ` + (black ? '!bg-black text-white hover:shadow-xl transition-all duration-500' : 'border text-neutral-600')} {...props}>
     {children}
-    <Icon className="text-white -ml-5 mt-0.5 opacity-0 group-hover:ml-2 group-hover:opacity-100 transition-all duration-100" />
+    {withIcon && <Icon name={withIcon} className="text-white -ml-5 mt-0.5 opacity-0 group-hover:ml-2 group-hover:opacity-100 transition-all duration-100" />}
   </button>
 }
