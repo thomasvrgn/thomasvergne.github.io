d363c008e02cabb031ac7d7398883293bfca7f8a
thomasvergne <thomas.vergne.contact@gmail.com>
2022-03-13 11:33:49 +0100

    🔧 Added react router.

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-import Projects from './Projects';
 import 'styles/tailwind.css';
-import Button from 'components/Button';
-import Blog from './Blog';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Main from './main/Index';
+import List from './blog/List';
+import Article from './blog/Article';
+import Error from './Error';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    <Projects />
-    <Blog />
-    <div className="p-16">
-      <Button onClick={() => window.localStorage.clear()}>
-        Supprimer le cache
-      </Button>  
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Main />} />
+          <Route path="blog">
+            <Route index element={<List />} />
+            <Route path=":slug" element={<Article />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <footer className="h-[750px] relative overflow-hidden">
+        {/* Grid effect */}
+        <div className="absolute w-full h-full grid-effect opacity-50"></div>
+        {/* Bottom blur effect */}
+        <div className="absolute -top-96 sm:-top-1/3 -left-1/3 -right-1/3 h-[35rem] bg-white blur-[3rem] sm:blur-[7rem] filter z-10"></div>
+      </footer>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
