3f1fd8853f794d4714520e4c5fdfdefcf90c651d
thomasvergne <thomas.vergne.contact@gmail.com>
2022-03-13 11:32:41 +0100

    ✨ Navbar component added.

---
+++ components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { IconBrandTwitter } from '@tabler/icons';
+import { IconBrandGithub } from '@tabler/icons';
+import { Link } from 'react-router-dom';
+
+export default function Navigation() {
+  return <nav className="container mx-auto p-8 md:pt-8 flex flex-col md:flex-row items-center 2xl:w-4/5">
+    <div className="flex flex-initial flex-col md:flex-row items-center text-center md:text-left">
+      <img className="w-20 h-20 object-cover rounded-full shadow-md" src="https://cdn.discordapp.com/attachments/768087687655194675/949965497301106729/66BB0FC6-BA56-46CF-83BD-1305865FEBB8.jpg" alt="" />
+      <div className="flex flex-col md:ml-4 justify-center">
+        <h1 className="font-black text-2xl text-black">
+          thomas vergne
+        </h1>
+        <Link to="/" className="text-lg -mt-1 text-neutral-500 hover:underline underline-offset-1">
+          Retourner à la page d'accueil
+        </Link>
+      </div>
+    </div>
+    <div className="flex flex-auto mt-8 md:mt-0 justify-end">
+      <ul className="flex gap-x-8 children:text-5xl">
+        <IconBrandGithub size={30} />
+        <IconBrandTwitter size={30} />
+      </ul>
+    </div>
+  </nav>;
+}
