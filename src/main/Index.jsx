4c53c968a85bd84b109b4381a49a7054942c064c
thomasvergne <thomas.vergne.contact@gmail.com>
2022-03-13 11:33:12 +0100

    ✨ Index page added.

---
+++ src/main/Index.jsx
@@ -0,0 +1,11 @@
+import Header from './Header';
+import Projects from './Projects';
+import Blog from './Blog';
+
+export default function Main() {
+  return <>
+    <Header />
+    <Projects />
+    <Blog />
+  </>
+}
