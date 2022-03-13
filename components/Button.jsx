0e6f7fc0c7ad333131d2b87d68d2535795ccac1c
thomasvergne <thomas.vergne.contact@gmail.com>
2022-03-13 11:32:15 +0100

    📱 Button and typo responsive added.

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,8 +1,7 @@
-import Icon from 'components/Icon';
-
 export default function Button({ children, withIcon, black, ...props }) {
-  return <button className={`group rounded-md py-2 px-8 flex items-center font-medium tracking-wide ` + (black ? '!bg-black text-white hover:shadow-xl transition-all duration-500' : 'border text-neutral-600')} {...props}>
-    {children}
-    {withIcon && <Icon name={withIcon} className="text-white -ml-5 mt-0.5 opacity-0 group-hover:ml-2 group-hover:opacity-100 transition-all duration-100" />}
+  const Icon = withIcon;
+  return <button className={`group rounded-md w-full sm:w-auto py-2 px-8 flex items-center font-medium tracking-wide ` + (black ? '!bg-black text-white hover:shadow-xl transition-all duration-500' : 'border text-neutral-600')} {...props}>
+    <span className="mx-auto" children={children} />
+    {withIcon && <Icon className="text-white -ml-5 mt-0.5 opacity-0 group-hover:ml-2 group-hover:opacity-100 transition-all duration-100" />}
   </button>
 }
--- components/Typography.jsx
+++ components/Typography.jsx
@@ -1,7 +1,7 @@
 export const HeadTitle = ({ children }) =>
-  <h2 className="font-black text-5xl text-black flex items-center">
+  <h2 className="font-black justify-center sm:justify-start text-4xl sm:text-5xl text-black flex items-center">
     {children}
-    <span className="block h-0.5 flex-auto mt-2 ml-16 bg-black opacity-10"></span>
+    <span className="hidden sm:block h-0.5 flex-auto mt-2 ml-16 bg-black opacity-10"></span>
   </h2>;
 
 export const Title = ({ children }) =>
